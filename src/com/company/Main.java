1126dd454b8cf659d437fe84b9a7067cb2890d4b
julianpettinari <julianpettinari@gmail.com>
2021-10-20 19:00:31 -0300

    se carga primera case con un if

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+      /*  valida si el primer numero es divisible por el segundo*/
+	int numero1 = 9;
+    int numero2 = 5;
+    if(numero1 % numero2 == 0){
+        System.out.println("el numero es divisible");
+
+    }else{
+        System.out.println("el numero no es divisible");
+    }
+
+    }
+}
